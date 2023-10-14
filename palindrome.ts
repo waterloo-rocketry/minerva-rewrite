90a9c2f555873246b6cf72715b26db258cfb081e
Shirley Fang <fangyixuan0123@gmail.com>
2023-10-14 11:22:53 -0400

    Revert "Added temporary placeholder test"
    This reverts commit ccf4400fa9f4c634f25517cc790d964c6dcbde63.

--- palindrome.ts
+++
@@ -1,5 +0,0 @@
-export const isPalindrome = (s: string): boolean => {
-    const cleanedString = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
-    const reversedString = cleanedString.split('').reverse().join('');
-    return cleanedString === reversedString;
-};
