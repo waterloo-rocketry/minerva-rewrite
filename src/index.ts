c2fce38c62a195c574107ca071e69c8a121ad6ca
wxp02 <wxp0025@gmail.com>
2023-11-01 22:47:16 -0400

    Testing Google Calendar API Connection

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
-console.log('There is nothing here yet.');
-console.error('This is an error.');
+import { OAuth2Client } from 'google-auth-library';
+import { google } from 'googleapis';
+const calendar = google.calendar('v3');
+import { config } from 'dotenv';
+
+config();
+
+// Set up OAuth2 client
+const auth = new OAuth2Client({
+  clientId: process.env.GOOGLE_ACCOUNT_CLIENT,
+  clientSecret: process.env.GOOGLE_ACCOUNT_SECRET,
+  redirectUri: 'https://developers.google.com/oauthplayground',
+});
+
+auth.setCredentials({
+  refresh_token: process.env.GOOGLE_ACCOUNT_TOKEN,
+});
+
+(async () => {
+  const result = await calendar.events.list({
+    auth: auth,
+    calendarId: 'primary',
+    timeMin: new Date().toISOString(),
+    maxResults: 1,
+    singleEvents: true,
+    orderBy: 'startTime',
+  });
+  console.log(result);
+})();
+
+console.log('Done!');
 while (true) {}
