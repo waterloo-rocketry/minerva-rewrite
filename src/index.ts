35628403beeb7bceb9b80f3a959bef00c4843a1a
Chamath Wijesekera <chamath2.wijesekera@gmail.com>
2023-11-02 12:31:06 -0400

    Add logging for deployment commit hash

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+console.log(
+  `Application minerva-dev (re)started. Deployed commit: ${process.env.COMMIT_HASH}`,
+);
+
 console.log('There is nothing here yet.');
 console.error('This is an error.');
 while (true) {}
