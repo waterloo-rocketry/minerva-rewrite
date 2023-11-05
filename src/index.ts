d129cabb0da2c9416eb5f5c5c4f3bb6f1438accf
yaobohan <b29yao@uwaterloo.ca>
2023-11-05 10:31:15 -0500

    move initial code from app.ts to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
+import { App } from '@slack/bolt';
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+(async () => {
+  // Start your app
+  await app.start();
+
+  console.log(`⚡️ Bolt app is running on port!`);
+})();
+
+
 console.log('There is nothing here yet.');
 console.error('This is an error.');
 while (true) {}
