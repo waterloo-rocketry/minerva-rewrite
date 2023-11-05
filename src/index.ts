2104fda77be4d63bcf8e8a973e77b47427a2fd3c
yaobohan <b29yao@uwaterloo.ca>
2023-11-05 12:35:46 -0500

    token name correction

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ config();
 
 // Initializes your app with your bot token and signing secret
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
+  token: process.env.SLACK_OAUTH_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
