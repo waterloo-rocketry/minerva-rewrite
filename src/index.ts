625a5ffe4d9eced4abf3574117661c20816f5aeb
yaobohan <b29yao@uwaterloo.ca>
2023-11-05 12:14:00 -0500

    added .env config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { App } from '@slack/bolt';
 
+import { config } from 'dotenv';
+
+config();
+
 // Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -8,11 +12,11 @@ const app = new App({
 
 (async () => {
   // Start your app
-  await app.start();
+  const port = process.env.PORT || 3000; // Use a port from the environment or default to 3000
+  await app.start(port);
 
   console.log(`⚡️ Bolt app is running on port!`);
 })();
-
 
 console.log('There is nothing here yet.');
 console.error('This is an error.');
