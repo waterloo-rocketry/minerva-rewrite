5585399feffbf7f8bdcb8edd13c58773bd1886d2
yaobohan <b29yao@uwaterloo.ca>
2023-11-05 22:52:37 -0500

    added message functionality

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,32 @@
 import { App } from '@slack/bolt';
-
 import { config } from 'dotenv';
 
 config();
 
-// Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_OAUTH_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  // You may remove the 'port' option if you're using only socketMode and no HTTP
+});
+
+app.message('hello', async ({ message, say }) => {
+  // Check if 'message' is properly typed, as 'user' might not be a direct property.
+  // If 'message' is of type 'MessageEvent', it should have a 'user' property.
+  if ('user' in message) {
+    await say(`Hey there <@${message.user}>!`);
+  } else {
+    console.error('The message event did not have a user property.');
+  }
 });
 
 (async () => {
-  // Start your app
-  const port = process.env.PORT || 3000; // Use a port from the environment or default to 3000
-  await app.start(port);
-
-  console.log(`⚡️ Bolt app is running on port ${port}!`);
+  try {
+    // Start your app
+    await app.start();
+    console.log(`⚡️ Bolt app is running!`);
+  } catch (error) {
+    console.error('Failed to start the Bolt app', error);
+  }
 })();
