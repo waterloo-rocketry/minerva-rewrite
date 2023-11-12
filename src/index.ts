85c8734c9b8506c8726cfc0f9158ed97de72fbc7
Shirley Fang <fangyixuan0123@gmail.com>
2023-11-12 00:07:27 -0500

    Test: Simulate application failure

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 console.log('There is nothing here yet.');
 console.error('This is an error.');
+
+// Simulating a failure after a short delay
+setTimeout(() => {
+    throw new Error('Simulated failure');
+}, 1000);
+
 while (true) {}
