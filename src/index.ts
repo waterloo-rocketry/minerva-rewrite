17409459f916a7d52b57798cf22951cca0ddc71a
Shirley Fang <fangyixuan0123@gmail.com>
2023-11-13 23:18:45 -0500

    Final commit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 console.log('There is nothing here yet.');
 console.error('This is an error.');
-
-// Simulating a failure after a short delay
-setTimeout(() => {
-    throw new Error('Simulated failure');
-}, 1000);
-
 while (true) {}
