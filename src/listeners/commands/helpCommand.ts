b8b502487662af093e04b4207c7bde386544d857
yaobohan <b29yao@uwaterloo.ca>
2023-12-10 22:54:41 -0500

    changes based on review

--- src/listeners/commands/helpCommand.ts
+++ src/listeners/commands/helpCommand.ts
@@ -1,9 +1,7 @@
 import { slackBotToken } from '../../utils/env';
 import { Middleware, SlackCommandMiddlewareArgs } from '@slack/bolt';
 
-const README_URL =
-  'https://github.com/waterloo-rocketry/minerva-rewrite/blob/main/README.md';
-const message = `For more information, check out this ${README_URL}!`;
+const message = `For more information, check out <https://github.com/waterloo-rocketry/minerva-rewrite/blob/main/README.md|minerva's README>.`;
 
 export const helpCommandHandler: Middleware<
   SlackCommandMiddlewareArgs
