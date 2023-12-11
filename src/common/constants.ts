26274b7323b784d7009736193483725c8c9750a9
Chamath Wijesekera <chamath2.wijesekera@gmail.com>
2023-12-10 20:08:55 -0500

    Address PR reviews

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,4 +1,16 @@
 /**
  * The slack channels to use when the `default` channel is specified
  */
-export const defaultSlackChannels = ["general", "airframe", "propulsion"];
+export const defaultSlackChannels = [
+  "general",
+  "airframe",
+  "electrical",
+  "business",
+  "controls",
+  "software",
+  "propulsion",
+  "payload",
+  "flight-dynamics",
+  "infrastructure",
+  "recovery",
+];
