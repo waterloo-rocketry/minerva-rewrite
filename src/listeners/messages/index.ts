a7966385047e2fa9e08a1797abef8257864aa6f3
Shirley Fang <fangyixuan0123@gmail.com>
2023-12-26 19:05:28 -0500

    Add the testing feature to minerva

--- src/listeners/messages/index.ts
+++ src/listeners/messages/index.ts
@@ -1,9 +1,26 @@
 import { App } from '@slack/bolt';
 import helloMessageCallback from './helloMessage';
+import {
+  generateEmojiPair,
+  seedMessageReactions,
+} from '../../utils/slackEmojis';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const register = (app: App) => {
   app.message(/^(hi|hello|hey).*/, helloMessageCallback);
+
+  // Add a new message listener for reacting to messages with a certain pattern
+  app.message('Nice to meet you', async ({ message, say }) => {
+    try {
+      const emojis = await generateEmojiPair(app);
+
+      await seedMessageReactions(app, message.channel, emojis, message.ts);
+
+      await say(`Emoji reaction added to the message!`);
+    } catch (error) {
+      console.error('Failed to add emoji reaction:', error);
+    }
+  });
 };
 
 export default { register };
