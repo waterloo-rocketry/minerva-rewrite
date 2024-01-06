f857e13c02e04842df34291192f484fe25f7fa77
Shirley Fang <fangyixuan0123@gmail.com>
2024-01-06 00:18:33 -0500

    Include the test message in the register handlers

--- src/listeners/messages/index.ts
+++ src/listeners/messages/index.ts
@@ -1,9 +1,10 @@
 import { App } from "@slack/bolt";
-import { helloMessageCallback } from "./helloMessage";
+import { helloMessageCallback, niceToMeetYouCallback } from "./helloMessage";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const register = (app: App): void => {
   app.message(/^(hi|hello|hey).*/, helloMessageCallback);
+  app.message("Nice to meet you", niceToMeetYouCallback);
 };
 
 export default { register };
