a8b6d28ebb031fda74ce1cc7b1b04d0cd5466629
Chamath Wijesekera <chamath2.wijesekera@gmail.com>
2024-01-10 15:20:21 -0500

    Remove debugging message listeners

--- src/listeners/messages/index.ts
+++ src/listeners/messages/index.ts
@@ -1,11 +1,8 @@
 import { App } from "@slack/bolt";
-import helloMessageCallback from "./helloMessage";
-import niceToMeetYouCallback from "./niceToMeetYouMessage";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const register = (app: App): void => {
-  app.message(/^(hi|hello|hey).*/, helloMessageCallback);
-  app.message("Nice to meet you", niceToMeetYouCallback);
+  // app.message(/^(hi|hello|hey).*/, helloMessageCallback);
 };
 
 export default { register };
