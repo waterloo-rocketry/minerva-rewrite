becca3705be08c2998691ff13c891fe902dc71c6
Chamath Wijesekera <chamath2.wijesekera@gmail.com>
2024-02-05 15:25:37 -0500

    Remove error testing from help command

--- src/listeners/commands/helpCommand.ts
+++ src/listeners/commands/helpCommand.ts
@@ -1,7 +1,6 @@
 import { Middleware, SlackCommandMiddlewareArgs } from "@slack/bolt";
 import { logCommandUsed } from "../../utils/logging";
 import { postEphemeralMessage } from "../../utils/slack";
-import { SlackLogger } from "../../classes/SlackLogger";
 
 const message = `For more information, check out <https://github.com/waterloo-rocketry/minerva-rewrite/blob/main/README.md|minerva's README>.`;
 
@@ -9,14 +8,5 @@ export const helpCommandHandler: Middleware<SlackCommandMiddlewareArgs> = async 
   await ack();
   await logCommandUsed(command);
 
-  try {
-    await postEphemeralMessage(client, command.channel_id, command.user_id, message);
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    throw new Error("This is a test error");
-  } catch (error) {
-    SlackLogger.getInstance().error(
-      `Failed to post ephemeral message to user \`${command.user_id}\` with error:`,
-      error,
-    );
-  }
+  await postEphemeralMessage(client, command.channel_id, command.user_id, message);
 };
