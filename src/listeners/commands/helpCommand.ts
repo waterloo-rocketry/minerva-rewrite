d9dd6d2807274fd20056634112c5576bab3f42b5
Chamath Wijesekera <chamath2.wijesekera@gmail.com>
2024-02-07 12:11:51 -0500

    Refactor logging for Slack API calls

--- src/listeners/commands/helpCommand.ts
+++ src/listeners/commands/helpCommand.ts
@@ -1,12 +1,21 @@
-import { Middleware, SlackCommandMiddlewareArgs } from "@slack/bolt";
+import { AllMiddlewareArgs, SlackCommandMiddlewareArgs } from "@slack/bolt";
 import { logCommandUsed } from "../../utils/logging";
 import { postEphemeralMessage } from "../../utils/slack";
+import { SlackLogger } from "../../classes/SlackLogger";
 
 const message = `For more information, check out <https://github.com/waterloo-rocketry/minerva-rewrite/blob/main/README.md|minerva's README>.`;
 
-export const helpCommandHandler: Middleware<SlackCommandMiddlewareArgs> = async ({ command, ack, client }) => {
-  await ack();
-  await logCommandUsed(command);
+export async function helpCommandHandler({
+  command,
+  ack,
+  client,
+}: SlackCommandMiddlewareArgs & AllMiddlewareArgs): Promise<void> {
+  ack();
+  logCommandUsed(command);
 
-  await postEphemeralMessage(client, command.channel_id, command.user_id, message);
-};
+  try {
+    await postEphemeralMessage(client, command.channel_id, command.user_id, message);
+  } catch (error) {
+    SlackLogger.getInstance().error("Failed to send help message", error);
+  }
+}
