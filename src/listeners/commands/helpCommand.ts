147ab92869a56169de202d0dcccbf41d4fdf678e
Chamath Wijesekera <chamath2.wijesekera@gmail.com>
2024-02-23 14:13:04 -0500

    Change bullet character

--- src/listeners/commands/helpCommand.ts
+++ src/listeners/commands/helpCommand.ts
@@ -6,8 +6,8 @@ import { postEphemeralMessage } from "../../utils/slack";
 import { SlackLogger } from "../../classes/SlackLogger";
 
 const message = `For guides on how to use minerva's functionality, check out the feature reference guides in the Google Drive (<https://drive.google.com/drive/u/1/folders/1h7FZKuF3Zvv8EygjB8pSTk2aGuHHpBtj|link>) 
-- <https://docs.google.com/document/d/1KbeJtU06Uosjpd3XlvEGtElOx5lAZz9vDe8yzHyFhvU/edit#heading=h.g3pdvke98aqf|Meeting Reminders>
-- <https://docs.google.com/document/d/1otYHtmFHzkN8g7089EGQpxi9-7C_iKFhDt2L2tuwxHs/edit#heading=h.8w6auka00s3p|Message Notifications>
+* <https://docs.google.com/document/d/1KbeJtU06Uosjpd3XlvEGtElOx5lAZz9vDe8yzHyFhvU/edit#heading=h.g3pdvke98aqf|Meeting Reminders>
+* <https://docs.google.com/document/d/1otYHtmFHzkN8g7089EGQpxi9-7C_iKFhDt2L2tuwxHs/edit#heading=h.8w6auka00s3p|Message Notifications>
 `;
 
 /**
