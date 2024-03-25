2da59b99f08ec0ac5fda4521b98c190dcb761c0c
Tintin <oofedindeed@gmail.com>
2024-03-25 03:13:37 -0400

    Testing notify command code, commit #1

---
+++ src/listeners/commands/notifyCommand.ts
@@ -0,0 +1,12 @@
+import { Middleware, SlackCommandMiddlewareArgs } from "@slack/bolt";
+import { logCommandUsed } from "../../utils/logging";
+import { postEphemeralMessage } from "../../utils/slack";
+
+const message = `For more information, check out <https://github.com/waterloo-rocketry/minerva-rewrite/blob/main/README.md|minerva's README>.`;
+
+export const notifyCommandHandler: Middleware<SlackCommandMiddlewareArgs> = async ({ command, ack, client }) => {
+    await ack();
+    await logCommandUsed(command);
+
+    await postEphemeralMessage(client, command.channel_id, command.user_id, message);
+};
