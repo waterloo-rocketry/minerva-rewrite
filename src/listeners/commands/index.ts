557e65ca6a75cd12515cd0b0e08489f1d0ee8fc1
Tintin <oofedindeed@gmail.com>
2024-03-30 02:32:20 -0400

    Testing notify

--- src/listeners/commands/index.ts
+++ src/listeners/commands/index.ts
@@ -1,10 +1,12 @@
 // commands/index.ts
 import { App } from "@slack/bolt";
 import { helpCommandHandler } from "./helpCommand";
+import { notifyCommandHandler } from "./notifyCommand";
 
 const register = (app: App): void => {
   app.command("/help", helpCommandHandler);
   // Other command registrations would go here
+  app.command("/notify", notifyCommandHandler);
 };
 
 export default { register };
